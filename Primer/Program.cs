51887d236c86b289525b41889b73f1ad78a286d5
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-01-25 17:15:13 -0400

    first commit

---
+++ Primer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hola mundo :3");
+        int.TryParse(Console.ReadLine(), out int numero);
+        System.Console.WriteLine(numero);
+    }
+}
