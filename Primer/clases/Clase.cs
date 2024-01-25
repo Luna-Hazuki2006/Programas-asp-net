7452b1fafbbe7ad91ee0a5f11357d392c07113ff
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-01-25 17:24:47 -0400

    aaaa

---
+++ Primer/clases/Clase.cs
@@ -0,0 +1,14 @@
+namespace clases.Clase
+{
+    public class Clase
+    {
+        public string Nombre { get; set; }
+        public string Area { get; set; }
+        public string Tipo { get; set; }
+        public int Edad { get; set; }
+        public double Experiencia { get; set; }
+        public int Resistencia { get; set; }
+        public int Ataque { get; set; }
+    }
+    
+}
